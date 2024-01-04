6b4e6e16f02f65bdd1732770f288eac045472546
vpp090 <vasil.petrov090@gmail.com>
2024-01-04 13:46:15 +0200

    push to a github repo

---
+++ ISpecialMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace SpecMapperR
+{
+    public interface ISpecialMapper 
+    {
+        TDest MapProperties<TSource, TDest>(TSource source, TDest dest) where TDest : new();
+    }
+}
---
+++ SpecialMapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace SpecMapperR
+{
+    public class SpecialMapper : ISpecialMapper
+    {
+        public TDest MapProperties<TSource, TDest>(TSource source, TDest dest) where TDest : new()
+        {
+            try
+            {
+                dest ??= new TDest();
+
+                PropertyInfo[] sourceProps = source.GetType().GetProperties();
+                PropertyInfo[] destProps = dest.GetType().GetProperties();
+
+                foreach (var sourceProp in sourceProps)
+                {
+                    var matchinProp = destProps.FirstOrDefault(p => p.Name == sourceProp.Name);
+                    if (matchinProp != null)
+                    {
+                        object value = sourceProp.GetValue(source);
+                        matchinProp.SetValue(dest, value);
+                    }
+                }
+
+                return dest;
+            }
+            catch(Exception)
+            {
+                throw;
+            } 
+        }
+    }
+}
